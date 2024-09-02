96ecf7b99b6921db2f9cf9a9d4a80af02dcb06a4
keerthanjk <keerthannani2525@gmail.com>
2024-09-02 11:09:29 +0530

    message

---
+++ Employee_Capstone/src/main/java/com/capstone/service/OpenAPIConfig.java
@@ -0,0 +1,28 @@
+package com.capstone.service;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("API Gateway")
+                            .version("1.0")
+                            .description("Documentation for API Gateway v1.0"))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("bearerAuth",
+                    new SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
